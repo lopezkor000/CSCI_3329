5f02eb7be018b20abc98a09111a58acbf73c464a
Kory <koriellopez03@gmail.com>
2023-11-05 21:23:08 -0600

    diff screens + buttons

--- frontend/selection.py
+++ frontend/selection.py
@@ -1,14 +1,35 @@
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
-from kivy.uix.widget import Widget
+# from kivy.uix.widget import Widget
+# from kivy.uix.boxlayout import BoxLayout
 
 
-class SelectionScreen(Widget):
+
+class BoxLayoutEx(Screen):
+    num = 0
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = 'vertical'
+    #     b1 = Button(text='A')
+    #     b2 = Button(text='B')
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    def on_click(self):
+        self.manager.current = 'selection'
+
+
+class SelectionScreen(Screen):
     pass
 
 
 class Selection(App):
     def build(self):
-        select = SelectionScreen()
-        return select
+        sm = ScreenManager()
+        sm.add_widget(BoxLayoutEx(name='example'))
+        sm.add_widget(SelectionScreen(name='selection'))
+        # select = SelectionScreen()
+        return sm
 
-Selection().run()
+
+if __name__ == '__main__':
+    Selection().run()
