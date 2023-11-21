c45462f81afa732ac1adb7fd0dea03cf00f27c84
Kory <koriellopez03@gmail.com>
2023-11-21 17:04:30 -0600

    Updated locations for CSV

--- backend/locations.py
+++ backend/locations.py
@@ -1,57 +1,36 @@
-"""
-Locations Class
-
-    constructor:
-    - list of locations (empty)
-        
-    functions:
-    - add location
-        [adds location to list of locations]
-
-    - remove location
-        [removes location in list of locations]
-
-    - label location
-        [includes location in category dict, labeling it]
-"""
 import csv
 
 class Locations:
-    def __init__(self, csvFile='locations.csv'):
-        self.csvFile = csvFile
-        self.locationsList = []  # 
-        self.loadLocations()  # 
+    def __init__(self):
+        self.csvFile = 'locations.csv'
+        self.loadLocations()
 
     def loadLocations(self):
+        """
+        Makes sure the CSV file exists, otherwise makes one
+        """
+        
         try:
-            with open(self.csvFile, mode='r', newline='', encoding='utf-8') as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    location = {
-                        'Name': row['Name'],
-                        'FoodType': row['FoodType']
-                    }
-                    self.locationsList.append(location)
+            with open(self.csvFile, mode='r') as file:
+                pass
         except FileNotFoundError:
-            print(f"The file {self.csvFile} was not found. Creating a new one with default headers.")
-            self.saveLocations()
+            self.CreateCSV()
 
-    def saveLocations(self):
-        with open(self.csvFile, mode='w', newline='', encoding='utf-8') as file:
-            fieldnames = ['Name', 'FoodType']  # 
+    def addLocation(self, name, *args):
+        """
+        Appends a new place to eat and adds appropriate categories
+        """
+
+        newLocation = [name, list(args)]
+        with open(self.csvFile, mode='a', newline='') as file:
+            csv.writer(file).writerow(newLocation)
+
+    def CreateCSV(self):
+        """
+        Creates new empty CSV with headers to track our places to eat
+        """
+
+        with open(self.csvFile, mode='w', newline='') as file:
+            fieldnames = ['Name', 'FoodType']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
-            for location in self.locationsList:
-                writer.writerow(location)
-
-    def addLocation(self, name, foodType):
-        newLocation = {
-            'Name': name,
-            'FoodType': foodType
-        }
-        self.locationsList.append(newLocation)
-        self.saveLocations()
-        print(f"Location {name} added to the list.")
-
-# Usage example
-locations = Locations()  # This will automatically load data from 'locations.csv'
