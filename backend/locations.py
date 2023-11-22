72aa6a7a0dba75172e5b847721468b1784cae674
Kory <koriellopez03@gmail.com>
2023-11-21 18:48:00 -0600

    updated locations and selection

--- backend/locations.py
+++ backend/locations.py
@@ -9,7 +9,7 @@ class Locations:
         """
         Makes sure the CSV file exists, otherwise makes one
         """
-        
+
         try:
             with open(self.csvFile, mode='r') as file:
                 pass
@@ -21,16 +21,38 @@ class Locations:
         Appends a new place to eat and adds appropriate categories
         """
 
-        newLocation = [name, list(args)]
+        newLocation = [x for x in args]
+        newLocation.insert(0, name)
         with open(self.csvFile, mode='a', newline='') as file:
             csv.writer(file).writerow(newLocation)
+    
+    def removeLocation(self, name):
+        """
+        Looks for first instance of location then recreates CSV without it
+        """
+        
+        locs = self.listedLocs()
+        for i, loc in enumerate(locs):
+            if loc[0] == name:
+                locs.pop(i)
+                break
+        self.CreateCSV(locs)
 
-    def CreateCSV(self):
+    def CreateCSV(self, locs=None):
         """
         Creates new empty CSV with headers to track our places to eat
         """
 
         with open(self.csvFile, mode='w', newline='') as file:
-            fieldnames = ['Name', 'FoodType']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
+            writer = csv.writer(file)
+            if locs != None:
+                for loc in locs:
+                    writer.writerow(loc)
+    
+    def listedLocs(self) -> list:
+        """
+        Returns listed rows of locations in CSV
+        """
+
+        with open(self.csvFile, mode='r') as file:
+            return [place for place in csv.reader(file)]
--- backend/selection.py
+++ backend/selection.py
@@ -1,55 +1,16 @@
-"""
-Selection Class
-    constructor:
-    - # of people choosing {passed in}
-
-    functions:
-    - select mode
-        [waits for user to pick category or totally random]
-    
-    - total random
-        [takes all locations saved and picks a random one]
-    
-    - category random
-        [takes locations within label and picks a random one]
-    
-    - running function
-        [recursively calls itself for as many people that are participating]
-"""
-
 import random
-from locations import Locations  # Import the updated Locations class
+from locations import Locations
 
 class Selection:
     def __init__(self, numPeople):
         self.numPeople = numPeople
-        self.locationsManager = Locations()  # Locations class to manage CSV data
-        self.locations = self.locationsManager.locationsList  # Dictionaries from locations.csv
-
-    def selectMode(self, choice):
-        if choice == "totally random":
-            self.totalRandom()
-        elif choice == "category random":
-            self.categoryRandom()
-        else:
-            print("Invalid choice.")
+        self.locs = Locations()
 
     def totalRandom(self):
-        if self.locations:
-            randomLocation = random.choice(self.locations)
-            print(f"The totally random location is: {randomLocation['Name']}")
-        else:
-            print("No locations available.")
+        if self.locs.listedLocs():
+            return random.choice(self.locs.listedLocs())
 
-    def categoryRandom(self):
-        foodType = input("Enter food type: ")  # category refers to 'FoodType'
-        categoryLocations = [location for location in self.locations if location['FoodType'] == foodType]
+    def categoryRandom(self, foodType):
+        categoryLocations = [location for location in self.locs.listedLocs() if foodType in location]
         if categoryLocations:
-            randomLocation = random.choice(categoryLocations)
-            print(f"The randomly chosen location from the food type {foodType} is: {randomLocation['Name']}")
-        else:
-            print(f"No locations available for the food type {foodType}.")
-
-# Usage
-selection = Selection(numPeople=2)
-selection.selectMode("totally random")
+            return random.choice(categoryLocations)
